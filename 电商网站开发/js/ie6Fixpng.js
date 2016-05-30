9cd9d1e810ea00b2c3fdd944566ead3e43156de2
xifengxx <xifengxx@163.com>
2016-05-30 18:55:26 +0800

    新增：电商网站案例

---
+++ 电商网站开发/js/ie6Fixpng.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+DD_belatedPNG.fix('div,ur,ol,li,dt,dd,dl,sapan,img,a,em,strong,h1,h2,h3,h4,h5,h6,p,td');
+DD_belatedPNG.fix('button');
+DD_belatedPNG.fix('a');
+DD_belatedPNG.fix('h1');
+DD_belatedPNG.fix('h5');
+DD_belatedPNG.fix('form');
+DD_belatedPNG.fix('p img');
+DD_belatedPNG.fix('a img');
+DD_belatedPNG.fix('img');
+DD_belatedPNG.fix('input');
+DD_belatedPNG.fix('li');
+DD_belatedPNG.fix('td');
+DD_belatedPNG.fix('span');
+DD_belatedPNG.fix('table tr td');
